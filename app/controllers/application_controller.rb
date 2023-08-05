17a980f6fec547025e5e1a45cbeb4790f3f124bf
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-08-05 05:39:20 +0000

    新規会員登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number])
+  end
+
   def after_sign_in_path_for(resource)
    admin_path
   end
