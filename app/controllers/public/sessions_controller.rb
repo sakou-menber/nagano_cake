39da27840386e9946cb670c05ae640ae545d836c
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-08-08 12:03:49 +0000

    退会機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -18,7 +18,15 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
+      redirect_to new_customer_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
