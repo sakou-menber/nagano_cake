6e7888a4d7ff41907aebb20c33fb5ddfac1bd58a
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-08-09 09:35:04 +0000

    退会機能

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,7 +20,7 @@ class Public::CustomersController < ApplicationController
      @customer.update(is_deleted: true)
      reset_session
      flash[:notice] = "退会処理を実行いたしました"
-     redirect_to root_path
+     redirect_to new_customer_registration_path
    end
 
 
