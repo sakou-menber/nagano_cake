111a5097a02f7cf45d845f7075726d5a350b1910
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-08-24 12:49:14 +0000

    注文管理

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ scope module: :public do
   get '/orders' => 'orders#index'
   get '/orders/complete' =>'orders#complete'
   get '/orders/:id' => 'orders#show', as: :order
-
   
+
   post '/orders/confirm' => 'orders#confirm'
   post '/orders' => 'orders#create'
 
@@ -23,7 +23,7 @@ scope module: :public do
   resources :items, only: [:index, :show]
   delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
   resources :cart_items, only: [:index, :update, :destroy,:create]
-  
+
 end
 
   devise_for :customers, skip: [:passwords], controllers: {
@@ -35,16 +35,7 @@ end
 namespace :admin do
   # 管理者用
   get '/' => 'homes#top'
-  #get '/items' => 'admin/items#index'
-  #post '/admin/items' => 'admin/items#create'
-  # get 'admin/items/new' => 'admin/items#new'
-  # get 'admin/items/:id' => 'admin/items#show'
-  # get '/admin/items/:id/edit' => 'admin/items#edit'
-  # get 'admin/customers' => 'admin/customers#index'
-  # get '/admin/customers/:id' => 'admin/customers#show'
-  # get '/admin/customers/:id/edit' => 'admin/customers#edit'
-  get 'admin/orders/:id' => 'admin/orders#show'
-
+  get '/admin/orders/:id' => 'orders#show', as: :order
   resources :items, only: [:index, :new, :create, :show, :edit, :update]
   resources :customers, only: [:index, :show, :edit, :update]
 
